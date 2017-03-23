3fc106baf69f9554ce6eef2f5c6ffaeb044cb941
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-23 12:25:31 -0700

    Fixed build script, and prod config

--- web/webpack/web.prod.config.js
+++ web/webpack/web.prod.config.js
@@ -1,44 +1,60 @@
-const path = require('path');
-const webpack = require('webpack');
+"use strict";
 
+var path = require("path");
+var webpack = require("webpack");
+
+var definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
+});
 module.exports = {
-  entry: [
-    path.join(__dirname, '../../app/web/index'),
-  ],
-  output: {
-    path: path.join(__dirname, '../public/'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    loaders: [
-      // take all less files, compile them, and bundle them in with our js bundle
-      { test: /\.less$/, loader: 'style!css!autoprefixer?browsers=last 2 version!less' },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-        },
-      },
+    devtool: "eval-source-map",
+    entry: [
+        'babel-polyfill',
+        path.join(__dirname, '../../app/web/index'),
     ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        // Useful to reduce the size of client-side libraries, e.g. react
-        NODE_ENV: JSON.stringify('production'),
-        PLATFORM_ENV: JSON.stringify('web'),
-      },
-    }),
-    // optimizations
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
-  ],
+    output: {
+        path: path.join(__dirname, '../public'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    plugins: [
+        definePlugin
+    ],
+    module: {
+        loaders: [{
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /(node_modules|bower_components)/
+            },
+            {
+                test: /\.js?$/,
+                loader: "babel-loader",
+                exclude: /(node_modules|bower_components)/
+            }, {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }, {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader"
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+            }, {
+                test: /\.json$/,
+                loader: "json"
+            }, {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: "style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap&includePaths[]=node_modules/compass-mixins/lib"
+            }, {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    }
 };
